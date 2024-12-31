a2815bea553df940bcb42268d70dfd275d7feeb0
nileshdeshmukhSogeti <85751891+nileshdeshmukhSogeti@users.noreply.github.com>
2024-12-31 11:04:00 +0000

    NA

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-#cnx = st.connection("snowflake")
-#session = cnx.session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Write directly to the app
 #Header 
