2e71f17a79f6847e321f0f615cf6402464a4424c
nileshdeshmukhSogeti <85751891+nileshdeshmukhSogeti@users.noreply.github.com>
2024-12-31 12:37:27 +0000

    NA

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,53 @@
+# Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
-# Display the smoothie logo at the top
-logo_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Smoothie_logo.svg/1200px-Smoothie_logo.svg.png"  # Replace with your logo URL or local file path
-st.image(logo_url, width=200)  # Adjust the width as per your logo size
+# Write directly to the app
+#Header 
 
-# Title of the app
-st.title("Smoothie Selector")
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.write(
+    "Choose the fruits you want in your custom Smoothie!"
+)
 
-# Create a dropdown (selectbox) for the user to choose a smoothie type
-smoothie_choice = st.selectbox("Select a smoothie to view:", ["Mango Smoothie", "Berry Blast Smoothie", "Green Smoothie", "Tropical Smoothie"])
 
-import os
-import snowflake.connector
-from dotenv import load_dotenv
+name_on_order = st.text_input("Name on Smoothie")
+st.write("This name will be on Smoothie", name_on_order)
 
-# Load environment variables from .env file
-load_dotenv()
 
-# Snowflake connection details from environment variables
-user = os.getenv("SNOWFLAKE_USER")
-password = os.getenv("SNOWFLAKE_PASSWORD")
-account = os.getenv("SNOWFLAKE_ACCOUNT")
-warehouse = os.getenv("SNOWFLAKE_WAREHOUSE")
-database = os.getenv("SNOWFLAKE_DATABASE")
-schema = os.getenv("SNOWFLAKE_SCHEMA")
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))#**To select only specific col data from a table 
+#st.dataframe(data=my_dataframe, use_container_width=True) #**To draw the table using data from my_dataframe
+
+
+
+
+ingredients_list = st.multiselect (
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections = 5
+    )
+ingredients_string = '' 
+time_to_insert = '' 
+
+if ingredients_list: #Handle indent of visuals if ingredients_list is not null: then do everything below this line that is indented. 
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
+
+    ingredients_string = '' 
+    for fruit_chosen in ingredients_list: 
+        ingredients_string += fruit_chosen + ' ' #+= add text to earlier selected text in loop
+    #st.write(ingredients_string)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                values ('""" + ingredients_string + """','""" +name_on_order+"""')"""
+
+    #st.write(my_insert_stmt)
+    #st.stop() #This helps for troubleshotting code
+    time_to_insert = st.button('Submit Order')
+    
+if time_to_insert:
+    session.sql(my_insert_stmt).collect()
+    st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
