eaccce66ba99e0018249b1357bae42fa709a8530
nileshdeshmukhSogeti <85751891+nileshdeshmukhSogeti@users.noreply.github.com>
2024-12-31 08:19:41 +0000

    Conn

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+conn = st.connection("snowflake")
 
 # Write directly to the app
 #Header 
