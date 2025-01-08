410f27c589c978648dbb842d30940af8d1fc7798
nileshdeshmukhSogeti <85751891+nileshdeshmukhSogeti@users.noreply.github.com>
2025-01-08 10:00:16 +0000

    NA

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,14 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))#**To select only specific col data from a table 
-st.dataframe(data=my_dataframe, use_container_width=True) #**To draw the table using data from my_dataframe
+#st.dataframe(data=my_dataframe, use_container_width=True) #**To draw the table using data from my_dataframe
+#st.stop()
+
+# Convert the Snowpark Dataframe to a pandas dataframe so that we can use the LOC function
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
-
-
 
 ingredients_list = st.multiselect (
     'Choose up to 5 ingredients:'
@@ -41,6 +45,8 @@ if ingredients_list: #Handle indent of visuals if ingredients_list is not null: 
     ingredients_string = '' 
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + ' ' #+= add text to earlier selected text in loop
+        search_on = pd_df.loc[pd_df['FRUIT_NAME']== fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search is for ', fruit_chosen ,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width= True)
